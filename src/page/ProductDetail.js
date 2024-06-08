a096888bd7493a777ed4e4832cef3b52b7ebbe3e
geunseonkim <guensoen@gmail.com>
2024-06-08 18:57:37 +1200

    edit show stock

--- src/page/ProductDetail.js
+++ src/page/ProductDetail.js
@@ -69,9 +69,17 @@ const ProductDetail = () => {
               {size === "" ? "사이즈 선택" : size.toUpperCase()}
             </Dropdown.Toggle>
 
-            <Dropdown.Menu className="size-drop-down">
+            {/* <Dropdown.Menu className="size-drop-down">
               <Dropdown.Item>M</Dropdown.Item>
-            </Dropdown.Menu>
+            </Dropdown.Menu> */}
+
+            <Dropdown.Menu className="size-drop-down">
+                            {Object.entries(productDetail.stock).map(([option, qty]) => (
+                                <Dropdown.Item key={option} eventKey={option}>
+                                    {`${option.toUpperCase()}: ${qty} left`}
+                                </Dropdown.Item>
+                            ))}
+                        </Dropdown.Menu>
 
           </Dropdown>
           <div className="warning-message">
