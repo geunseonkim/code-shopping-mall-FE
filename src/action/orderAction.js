892f57ead3b935c62868c8d15642192e41b043c5
geunseonkim <guensoen@gmail.com>
2024-06-11 03:09:44 +1200

    create order and empty cart

--- src/action/orderAction.js
+++ src/action/orderAction.js
@@ -3,7 +3,22 @@ import * as types from "../constants/order.constants";
 import { cartActions } from "./cartAction";
 import { commonUiActions } from "./commonUiAction";
 
-const createOrder = (payload) => async (dispatch) => {};
+const createOrder = (payload, navigate) => async (dispatch) => {
+  try {
+    dispatch({ type: types.CREATE_ORDER_REQUEST });
+    const response = await api.post("/order", payload);
+    dispatch({
+      type: types.CREATE_ORDER_SUCCESS,
+      payload: response.data.orderNum,
+    });
+    console.log("payload", response);
+    dispatch(cartActions.getCartQty());
+    navigate("/payment/success");
+  } catch (err) {
+    dispatch({ type: types.CREATE_ORDER_FAIL, payload: err.error });
+    dispatch(commonUiActions.showToastMessage(err.error, "error"));
+  }
+};
 
 const getOrder = () => async (dispatch) => {};
 const getOrderList = (query) => async (dispatch) => {};
--- src/reducer/productReducer.js
+++ src/reducer/productReducer.js
@@ -2,39 +2,45 @@ import * as types from "../constants/product.constants";
 const initialState = {
   loading: false,
   error: "",
-  productList:[],
-  totalPageNum : 1,
+  productList: [],
+  totalPageNum: 1,
   productDetail: null,
-  selectedProduct: null
+  selectedProduct: null,
 };
 
 function productReducer(state = initialState, action) {
   const { type, payload } = action;
-  switch(type) {
+  switch (type) {
     case types.PRODUCT_CREATE_REQUEST:
     case types.PRODUCT_GET_REQUEST:
     case types.GET_PRODUCT_DETAIL_REQUEST:
     case types.PRODUCT_EDIT_REQUEST:
     case types.PRODUCT_DELETE_REQUEST:
-      return {...state, loading: false}
+      return { ...state, loading: true };
     case types.PRODUCT_CREATE_SUCCESS:
     case types.PRODUCT_EDIT_SUCCESS:
     case types.PRODUCT_DELETE_SUCCESS:
-      return {...state, loading: false, error: ""}
+      return { ...state, loading: false, error: "" };
     case types.PRODUCT_GET_SUCCESS:
-      return {...state, loading: false, error: "", productList: payload.data, totalPageNum: payload.totalPageNum}
+      return {
+        ...state,
+        loading: false,
+        error: "",
+        productList: payload.data,
+        totalPageNum: payload.totalPageNum,
+      };
     case types.GET_PRODUCT_DETAIL_SUCCESS:
-    return { ...state, loading: false, error: "", productDetail: payload};
+      return { ...state, loading: false, error: "", productDetail: payload };
     case types.PRODUCT_CREATE_FAIL:
     case types.PRODUCT_GET_FAIL:
     case types.GET_PRODUCT_DETAIL_FAIL:
     case types.PRODUCT_EDIT_FAIL:
     case types.GET_PRODUCT_DETAIL_FAIL:
-      return {...state, loading: false, error: payload}
+      return { ...state, loading: false, error: payload };
     case types.SET_SELECTED_PRODUCT:
-      return {...state, selectedProduct: payload}
+      return { ...state, selectedProduct: payload };
     default:
-    return state
+      return state;
   }
 }
 
